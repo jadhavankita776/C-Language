6189ab406bb26ac9c0b88c817727c4fbee2284a1
jadhavankita776 <jadhavankita839@gmail.com>
2024-12-24 17:03:05 +0530

    Create Hello world.c

---
+++ Hello world.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // Include standard input/output library
+
+int main() {
+    printf("Hello, World!\n"); // Print the message
+    return 0; // Return 0 to indicate successful execution
+}
